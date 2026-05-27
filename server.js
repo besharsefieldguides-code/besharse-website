bad04b3bcc5793c9c5aada624bb54f3ba61a50d5
besharsefieldguides-code <austinbmatthew1811@gmail.com>
2026-05-26 19:19:40 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.post("/api/admin-login", (req, res) => {
+  const { username, password } = req.body;
+
+  const adminUsername = process.env.ADMIN_USERNAME;
+  const adminPassword = process.env.ADMIN_PASSWORD;
+
+  if (username === adminUsername && password === adminPassword) {
+    return res.json({ success: true });
+  }
+
+  return res.json({ success: false });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
